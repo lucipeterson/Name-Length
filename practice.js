dae32cd09068cd46360e9333f98497b3966ac2b3
Luci Peterson <lucijpeterson@gmail.com>
2019-03-23 12:22:33 -0700

    Added essential files
    Added script and html files to repository.  Removed unneccessary comments from script file.

---
+++ practice.js
@@ -0,0 +1,14 @@
+let firstName = prompt("What is your first name?");
+let lastName = prompt("What is your last name?");
+let fullName = firstName.toUpperCase() + " " + lastName.toUpperCase();
+let nameLength = fullName.length;
+alert("The string " + fullName + " is " + nameLength + " characters long.")
+
+
+
+
+
+
+
+
+
